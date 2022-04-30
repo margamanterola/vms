25214dd1adc68048941be518cfaa0f3ff9f4fffb
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-30 13:32:41 +0800

    Delete vms directory

--- vms/organization/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.db import models
-from django.forms import ModelForm
-from organization.models import Organization
-
-class OrganizationForm(ModelForm):
-    class Meta:
-        model = Organization
-        fields = ['name']
